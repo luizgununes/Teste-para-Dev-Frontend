188bc9fc51dbde3b64c50b4a25e56a7753d5b5d7
luiz.nunes <luiz.nunes@quiver.net.br>
2020-09-15 00:21:59 -0300

    criação dos componentes

---
+++ amplifiqueme-teste/src/components/participantes-detalhes/participantes-detalhes.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-participantes-detalhes',
+  templateUrl: './participantes-detalhes.component.html',
+  styleUrls: ['./participantes-detalhes.component.less']
+})
+export class ParticipantesDetalhesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ amplifiqueme-teste/src/components/participantes/participantes.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-participantes',
+  templateUrl: './participantes.component.html',
+  styleUrls: ['./participantes.component.less']
+})
+export class ParticipantesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
